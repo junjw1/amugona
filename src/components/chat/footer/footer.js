c841c67ab72fd39bf4a88e55c1bda1489424a9db
junjw1 <junjw1@daum.net>
2016-07-26 12:07:30 +0900

    main, create, chat 각 컴포넌트 나누기

---
+++ src/components/chat/footer/footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import input_chat from './input_chat';
+
+
+class footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+      <input_chat/>
+      </div>
+    );
+  }
+}
+
+export default footer;
---
+++ src/components/chat/header/teamname.js
@@ -0,0 +1,14 @@
+//header.js > teamname.js 헤더의 팀이름
+import React, { Component } from 'react';
+
+class desc extends Component {
+  render() {
+    return (
+      <div className="desc">
+        <h1>desc</h1>
+      </div>
+    );
+  }
+}
+
+export default desc;
